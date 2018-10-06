40bf9fd32b82c8a8cef3bb4acc8a555f01bd9b7d
Tec Tale <samuelrmeyer@gmail.com>
2018-10-06 19:49:38 -0400

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SignInComponent } from "./components/sign-in/sign-in.component";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
+import { GalleryComponent } from "./components/gallery/gallery.component";
+import { ImageDetailComponent } from "./components/image-detail/image-detail.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ProfileComponent } from './components/profile/profile.component';
+import { UserService } from "./shared/user/user.service";
+
+import { appRoutes } from "./routes";
+
+import { AuthGuard } from "./auth/auth.guard";
+import { AuthInterceptor } from "./auth/auth.interceptor";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignInComponent,
+    SignUpComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // UserService,
+    // AuthGuard,
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   useClass: AuthInterceptor,
+    //   multi: true
+    // }
+  ],
+  bootstrap: [
+    AppComponent,
+    HeaderComponent,
+    SignInComponent,
+    SignUpComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    HomeComponent,
+    ProfileComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from "@angular/router";
+import { GalleryComponent } from "./components/gallery/gallery.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ImageDetailComponent } from "./components/image-detail/image-detail.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { AuthGuard } from "./auth/auth.guard";
+
+export const appRoutes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "images/:gallery/:page",
+    component: GalleryComponent
+  },
+  {
+    path: "imagedetail/:id",
+    component: ImageDetailComponent
+  },
+  {
+    path: "profile",
+    component: ProfileComponent
+  },
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  }
+];
